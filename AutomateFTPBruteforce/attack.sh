315cfe821058469c2e60c7ad7944f8a4d57da8af
davoodya <davoodya40@gmail.com>
2025-07-03 18:56:13 +0330

    apply changes into Automate FTP Bruteforcer

--- AutomateFTPBruteforce/attack.sh
+++ AutomateFTPBruteforce/attack.sh
@@ -4,8 +4,8 @@
 echo "[*] Waiting for FTP service..."
 sleep 3
 
-# Run the attack with ncx
+# Run the attack with nxc
 echo "[*] Running NetExec FTP test..."
-ncx ftp 127.0.0.1 -u testuser -p testpass > /results/ftp-pentest.txt
+nxc ftp 127.0.0.1 -u testuser -p testpass > /results/ftp-pentest.txt
 
 echo "[*] Scan complete. Result saved to /results/ftp-pentest.txt"
